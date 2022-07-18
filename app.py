c9261adbbda0cb46e23ccb117ca390101e376aae
rohanbagde001 <rohanbagde001@gmail.com>
2022-07-18 08:52:40 +0530

    wrote custom exception and tested

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from thyroid.logger import logging
+from thyroid.exception import ThyroidException
+import sys
+
 
 
 app=Flask(__name__)
@@ -7,7 +10,12 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("testing logging functionality")
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        thyroid = ThyroidException(e, sys)
+        logging.info(thyroid.error_message)
+        logging.info("testing logging functionality")
     return "CI CD pipeline has been established."
 
 
